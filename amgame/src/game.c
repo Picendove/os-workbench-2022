3b75fc3e9df84869f8029f46aa868c3549ad4930
tracer-nju <tracer@nju.edu.cn>
2022-10-19 15:37:14 +0800

    Linux go-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:37:14 up 36 min,  1 user,  load average: 0.17, 0.33, 0.37

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -22,11 +22,13 @@ int main(const char *args) {
   puts("Press any key to see its key code...\n");
 
   while (1) {
-    print_key();
     AM_INPUT_KEYBRD_T event = { .keycode = AM_KEY_NONE };
     ioe_read(AM_INPUT_KEYBRD, &event);
     if (event.keycode != AM_KEY_NONE && event.keydown) {
       puts("Key pressed: ");
+      if(strcmp(key_names1[event.keycode] ,"ESCAPE") == 0) {
+        return 0;
+      }
       switch(*key_names1[event.keycode])
       {
         case 'W': 
